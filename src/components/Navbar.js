b7e412a58598897a89b05358dbb6c6036f166794
IamDevTrivedi <devtrivedi22082008@gmail.com>
2024-09-21 22:02:17 +0530

    Navbar-Footer-Added

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className='bg-gradient-to-r from-slate-800  to-slate-900 text-white px-4 py-3 md:px-10 md:py-4 flex flex-col md:flex-row items-center justify-between shadow-lg'>
+      {/* Logo Section */}
+      <div className='flex items-center gap-4'>
+        <span className='text-4xl font-extrabold text-blue-400'>Text Tools</span>
+        <span className='hidden md:inline text-gray-300'>Manipulate Text Easily</span>
+      </div>
+
+      {/* Navigation Links */}
+      <div className='flex items-center gap-4 mt-4 md:mt-0'>
+        <button className='hover:bg-blue-600 focus:bg-blue-700 text-white font-semibold py-2 px-5 rounded transition duration-300 ease-in-out'>
+          Home
+        </button>
+        <button className='hover:bg-blue-600 focus:bg-blue-700 text-white font-semibold py-2 px-5 rounded transition duration-300 ease-in-out'>
+          About Us
+        </button>
+      </div>
+    </nav>
+  )
+}
